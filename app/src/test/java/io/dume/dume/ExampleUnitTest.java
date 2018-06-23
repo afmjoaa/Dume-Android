4036a6cf6a8831589fc308e0f5d714e193dba601
enam <surelastmail@gmail.com>
2018-06-23 12:27:40 +0600

    @Test case

--- app/src/test/java/io/dume/dume/ExampleUnitTest.java
+++ app/src/test/java/io/dume/dume/ExampleUnitTest.java
@@ -1,6 +1,12 @@
 package io.dume.dume;
 
+import android.util.Log;
+
 import org.junit.Test;
+
+import io.dume.dume.homepage.MainContract;
+import io.dume.dume.homepage.MainPresenter;
+import io.dume.dume.model.ModelSource;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +16,33 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void isMultiplyCorrect() {
+        assertEquals(5, multiply(3, 2));
+    }
+
+    public int multiply(int x, int y)
+
+    {
+        return x * y;
+    }
+
+
+    interface MockContract extends MainContract {
+
+    }
+
+    class MockPresenter extends MainPresenter {
+        public MockPresenter(MainContract.View view, ModelSource model) {
+            super(view, model);
+        }
+    }
+
+
 }
