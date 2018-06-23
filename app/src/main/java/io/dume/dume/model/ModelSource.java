a5a2d2c2a0c33b977db9cdf38a49ec21e53d48c3
enam <surelastmail@gmail.com>
2018-06-23 16:25:09 +0600

    Implemented Navigation Drawer

--- app/src/main/java/io/dume/dume/model/ModelSource.java
+++ app/src/main/java/io/dume/dume/model/ModelSource.java
@@ -8,13 +8,24 @@ import io.dume.dume.homepage.MainContract;
 public class ModelSource implements MainContract.Model {
     CallBack listener;
 
+    /**
+     * set a callback listener before calling model data its nullable
+     *
+     * @param listener
+     */
+    @Override
+    public void setListener(CallBack listener) {
+
+    }
+
+    /**
+     * it returns a random number bound of 5000
+     *
+     * @return int
+     */
     @Override
     public int getRandomNumber() {
         return new Random().nextInt(500);
     }
 
-    @Override
-    public void setListener(CallBack listener) {
-        this.listener = listener;
-    }
 }
